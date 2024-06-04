6f242aeabf8445dd7ff2a6bfabd067e67100878e
KhaiLe190904 <lequangkhai190904@gmail.com>
2024-06-04 20:00:51 +0700

    new commit

--- src/main/java/com/webbookingticket/fullstack/controller/SignInController.java
+++ src/main/java/com/webbookingticket/fullstack/controller/SignInController.java
@@ -2,11 +2,16 @@ package com.webbookingticket.fullstack.controller;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 
 @Controller
 public class SignInController {
     @GetMapping("/signin")
-    public String showSeat(){
-        return "Signin";
+    public String checkSignIn(@RequestParam(value = "userName", required = false) String userName,
+                              @RequestParam(value = "passWord", required = false) String passWord){
+        System.out.println("UserName: " + userName + "\n" + "Password: " + passWord);
+        return "Signin/Signin";
     }
 }
