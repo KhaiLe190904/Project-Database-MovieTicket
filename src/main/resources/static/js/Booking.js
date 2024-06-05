d85ec2abfe8bc83a83859963b5de4a89c357da21
kiwwi1 <dquanghuy221@gmail.com>
2024-06-05 08:28:32 +0700

    new commit

---
+++ src/main/resources/static/js/Booking.js
@@ -0,0 +1,37 @@
+let seats = document.querySelector(".all-seats");
+for (var i = 0; i < 59; i++) {
+  let randint = Math.floor(Math.random() * 2);
+  let booked = randint === 1 ? "booked" : "";
+  let disabled = booked === "booked" ? "disabled" : "";
+  
+  seats.insertAdjacentHTML(
+    "beforeend",
+    '<input type="checkbox" name="tickets" id="s' +
+      (i + 2) +
+      '" ' + disabled + ' /><label for="s' +
+      (i + 2) +
+      '" class="seat ' +
+      booked +
+      '"><span class="material-icons">event_seat</span></label>'
+  );
+}
+
+let tickets = seats.querySelectorAll("input:not(:disabled)");
+tickets.forEach((ticket) => {
+  ticket.addEventListener("change", () => {
+    let amount = document.querySelector(".amount").innerHTML;
+    let count = document.querySelector(".count").innerHTML;
+    amount = Number(amount);
+    count = Number(count);
+
+    if (ticket.checked) {
+      count += 1;
+      amount += 200;
+    } else {
+      count -= 1;
+      amount -= 200;
+    }
+    document.querySelector(".amount").innerHTML = amount;
+    document.querySelector(".count").innerHTML = count;
+  });
+});
