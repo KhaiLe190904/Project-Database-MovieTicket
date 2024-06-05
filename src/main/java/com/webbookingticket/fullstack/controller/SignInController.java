5247a9c3814fe6952f98b95bddd59c8ee79ef932
kiwwi1 <dquanghuy221@gmail.com>
2024-06-05 15:17:18 +0700

    Đm KhaiLe

--- src/main/java/com/webbookingticket/fullstack/controller/SignInController.java
+++ src/main/java/com/webbookingticket/fullstack/controller/SignInController.java
@@ -2,8 +2,8 @@ package com.webbookingticket.fullstack.controller;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
+// import org.springframework.web.bind.annotation.PostMapping;
+// import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 
 @Controller
